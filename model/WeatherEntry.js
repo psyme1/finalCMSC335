0a270b03a5ff6223460bc396663e5b4339edfafe
psyme1 <117768873+psyme1@users.noreply.github.com>
2025-12-10 00:53:11 -0500

    Update WeatherEntry.js

--- model/WeatherEntry.js
+++ model/WeatherEntry.js
@@ -1,25 +1,9 @@
-const express = require('express');
-const axios = require('axios');
-const Entry = require('./model/WeatherEntry');
-const router = express.Router();
+const mongoose = require('mongoose');
 
-router.get('/',  async (req, res) => {
-    res.render('index')
-});
-router.post('/weather', async (req, res) => {
-    const city = req.body.city;
-    try{
-        const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_KEY}&query=${city}`;
-        const out = await axios.get(url);
-        const data = out.data;
-        const entry = new Entry({
-            city: data.location.name, temperature: data.current.temperature, description: data.current.weather_descriptions[0]
-        });
-        await entry.save();
-        res.render('results', {weather: entry})
-    }catch(err){
-        res.send("Error getting weather data");
+const schema = new mongoose.Schema(
+    {
+        city: String, temperature: Number, description: String, data: {type: Date, default: Date.now}
     }
-})
+)
 
-module.exports = router;
+module.exports = mongoose.model('WeatherEntry', schema);
